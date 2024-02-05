9084b816394ef2ac0918b431e39fe609b9d452cc
Henry Leung <henryskyleung@gmail.com>
2024-02-05 10:48:06 -0500

    fix copied code producing extra blank lines

--- static/js/copy_code.js
+++ static/js/copy_code.js
@@ -8,37 +8,34 @@ function createCopyButton(highlightDiv) {
 }
 
 async function copyCodeToClipboard(button, highlightDiv) {
-  const codeToCopy = highlightDiv.querySelector(":last-child > .chroma > code").innerText;
+  // Get all the lines within the code block
+  const codeLines = Array.from(highlightDiv.querySelectorAll(":last-child > .chroma > code > .line"));
+
+  // Extract text from each line, excluding the line number
+  const codeFragments = codeLines.map(line => {
+    return Array.from(line.childNodes)
+      .filter(el => !el.classList || !el.classList.contains("ln"))
+      .map(el => el.textContent)
+      .join('');
+  });
+
+  // Join fragments and then split by double newline to remove extra newlines
+  let codeToCopy = codeFragments.join(''); // Join all fragments into a single string
+  codeToCopy = codeToCopy.replace(/\n{3,}/g, '\n\n'); // Replace three or more consecutive newlines with just two
+
   try {
-    result = await navigator.permissions.query({ name: "clipboard-write" });
+    const result = await navigator.permissions.query({ name: "clipboard-write" });
     if (result.state == "granted" || result.state == "prompt") {
       await navigator.clipboard.writeText(codeToCopy);
     } else {
-      copyCodeBlockExecCommand(codeToCopy, highlightDiv);
+      codeWasNOTCopied(button);
     }
-  } catch (_) {
-    copyCodeBlockExecCommand(codeToCopy, highlightDiv);
+  } catch (error) {
+    codeWasNOTCopied(button);
   }
   finally {
     codeWasCopied(button);
   }
-}
-
-function copyCodeBlockExecCommand(codeToCopy, highlightDiv) {
-  const textArea = document.createElement("textArea");
-  textArea.contentEditable = 'true'
-  textArea.readOnly = 'false'
-  textArea.className = "copyable-text-area";
-  textArea.value = codeToCopy;
-  highlightDiv.insertBefore(textArea, highlightDiv.firstChild);
-  const range = document.createRange()
-  range.selectNodeContents(textArea)
-  const sel = window.getSelection()
-  sel.removeAllRanges()
-  sel.addRange(range)
-  textArea.setSelectionRange(0, 999999)
-  document.execCommand("copy");
-  highlightDiv.removeChild(textArea);
 }
 
 function codeWasCopied(button) {
@@ -47,6 +44,14 @@ function codeWasCopied(button) {
   setTimeout(function() {
     button.innerText = "Copy";
   }, 2000);
+}
+
+function codeWasNOTCopied(button) {
+  button.blur();
+  button.innerText = "Error! Code NOT Copied";
+  setTimeout(function() {
+    button.innerText = "Copy";
+  }, 5000);
 }
 
 function addCopyButtonToDom(button, highlightDiv) {
