adf0b4e1aa8a86c130f69d0da333b394c79e8f5c
henrysky <henrysky@users.noreply.github.com>
2024-05-27 17:36:19 +0000

    deploy: 37beff3ac7df585d55a05dc3965208edb34e8045

---
+++ js/copy_code.js
@@ -0,0 +1,66 @@
+function createCopyButton(highlightDiv) {
+  const button = document.createElement("button");
+  button.className = "copy-code-button";
+  button.type = "button";
+  button.innerText = "Copy";
+  button.addEventListener("click", () => copyCodeToClipboard(button, highlightDiv));
+  addCopyButtonToDom(button, highlightDiv);
+}
+
+async function copyCodeToClipboard(button, highlightDiv) {
+  // Get all the lines within the code block
+  const codeLines = Array.from(highlightDiv.querySelectorAll(":last-child > .chroma > code > .line"));
+
+  // Extract text from each line, excluding the line number
+  const codeFragments = codeLines.map(line => {
+    return Array.from(line.childNodes)
+      .filter(el => !el.classList || !el.classList.contains("ln"))
+      .map(el => el.textContent)
+      .join('');
+  });
+
+  // Join fragments and then split by double newline to remove extra newlines
+  let codeToCopy = codeFragments.join(''); // Join all fragments into a single string
+  codeToCopy = codeToCopy.replace(/\n{3,}/g, '\n\n'); // Replace three or more consecutive newlines with just two
+
+  try {
+    const result = await navigator.permissions.query({ name: "clipboard-write" });
+    if (result.state == "granted" || result.state == "prompt") {
+      await navigator.clipboard.writeText(codeToCopy);
+    } else {
+      codeWasNOTCopied(button);
+    }
+  } catch (error) {
+    codeWasNOTCopied(button);
+  }
+  finally {
+    codeWasCopied(button);
+  }
+}
+
+function codeWasCopied(button) {
+  button.blur();
+  button.innerText = "Copied!";
+  setTimeout(function() {
+    button.innerText = "Copy";
+  }, 2000);
+}
+
+function codeWasNOTCopied(button) {
+  button.blur();
+  button.innerText = "Error! Code NOT Copied";
+  setTimeout(function() {
+    button.innerText = "Copy";
+  }, 5000);
+}
+
+function addCopyButtonToDom(button, highlightDiv) {
+  highlightDiv.insertBefore(button, highlightDiv.firstChild);
+  const wrapper = document.createElement("div");
+  wrapper.className = "highlight-wrapper";
+  highlightDiv.parentNode.insertBefore(wrapper, highlightDiv);
+  wrapper.appendChild(highlightDiv);
+}
+
+document.querySelectorAll(".highlight")
+  .forEach(highlightDiv => createCopyButton(highlightDiv));
